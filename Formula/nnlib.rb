6d9675c9aee2abef49e3cb99948231e4d97e344d
Luke Godfrey <thelukester92@me.com>
2017-11-21 11:57:25 -0600

    Updated nnlib version.

--- Formula/nnlib.rb
+++ Formula/nnlib.rb
@@ -1,8 +1,8 @@
 class Nnlib < Formula
 	desc "All-header library for building, training, and using neural networks."
 	homepage "https://github.com/thelukester92/nnlib/"
-	url "https://github.com/thelukester92/nnlib/archive/v2.0.7.tar.gz"
-	sha256 "ce069fc1075a36619cb00b290380b4a62619db75db2b53b1e26c6f9bb36c512f"
+	url "https://github.com/thelukester92/nnlib/archive/v2.0.8.tar.gz"
+	sha256 "a9a2bcb1b4937e4c53de2382f8f695770d7e4a80daa8d1c99ad794e03f8bff73"
 	head "https://github.com/thelukester92/nnlib.git", :branch => "develop"
 	
 	def install
