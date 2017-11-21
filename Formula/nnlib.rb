6bd9d2e420a4bf0991dd3b2f2dfd0c2a08860b54
Luke Godfrey <thelukester92@me.com>
2017-11-21 10:54:56 -0600

    Fixed SHA256.

--- Formula/nnlib.rb
+++ Formula/nnlib.rb
@@ -2,7 +2,7 @@ class Nnlib < Formula
 	desc "All-header library for building, training, and using neural networks."
 	homepage "https://github.com/thelukester92/nnlib/"
 	url "https://github.com/thelukester92/nnlib/archive/v2.0.7.tar.gz"
-	sha256 "f56e2d34de7e5f84cb4c5ecc944b947f102d4a37783bd3aca9ebbcbb41f80315"
+	sha256 "ce069fc1075a36619cb00b290380b4a62619db75db2b53b1e26c6f9bb36c512f"
 	head "https://github.com/thelukester92/nnlib.git", :branch => "develop"
 	
 	def install
